ffa6fbaecc266ee07d17861a11f215b46c9a7212
manikant153 <84517627+manikant153@users.noreply.github.com>
2024-02-19 13:13:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
-const body = document.querySelector("body"),
-    sidebar = body.querySelector(".sidebar"),
-    toggle = body.querySelector(".toggle"),
-    searchBtn = body.querySelector(".search-box"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+// Selecting the sidebar and buttons
+const sidebar = document.querySelector(".sidebar");
+const sidebarOpenBtn = document.querySelector("#sidebar-open");
+const sidebarCloseBtn = document.querySelector("#sidebar-close");
+const sidebarLockBtn = document.querySelector("#lock-icon");
 
-    toggle.addEventListener('click', ()=>{
-        sidebar.classList.toggle("close");
-    });
-    searchBtn.addEventListener('click', ()=>{
-        sidebar.classList.remove("close");
-    });
+// Function to toggle the lock state of the sidebar
+const toggleLock = () => {
+  sidebar.classList.toggle("locked");
+  // If the sidebar is not locked
+  if (!sidebar.classList.contains("locked")) {
+    sidebar.classList.add("hoverable");
+    sidebarLockBtn.classList.replace("bx-lock-alt", "bx-lock-open-alt");
+  } else {
+    sidebar.classList.remove("hoverable");
+    sidebarLockBtn.classList.replace("bx-lock-open-alt", "bx-lock-alt");
+  }
+};
 
+// Function to hide the sidebar when the mouse leaves
+const hideSidebar = () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.add("close");
+  }
+};
 
-    modeSwitch.addEventListener('click', ()=>{
-        body.classList.toggle("dark");
+// Function to show the sidebar when the mouse enter
+const showSidebar = () => {
+  if (sidebar.classList.contains("hoverable")) {
+    sidebar.classList.remove("close");
+  }
+};
 
-        if(body.classList.contains("dark")){
-            modeText.innerText = "Light Mode";
-        }else{
-            modeText.innerText = "Dark Mode";
-        }
-    });
+// Function to show and hide the sidebar
+const toggleSidebar = () => {
+  sidebar.classList.toggle("close");
+};
+
+// If the window width is less than 800px, close the sidebar and remove hoverability and lock
+if (window.innerWidth < 800) {
+  sidebar.classList.add("close");
+  sidebar.classList.remove("locked");
+  sidebar.classList.remove("hoverable");
+}
+
+// Adding event listeners to buttons and sidebar for the corresponding actions
+sidebarLockBtn.addEventListener("click", toggleLock);
+sidebar.addEventListener("mouseleave", hideSidebar);
+sidebar.addEventListener("mouseenter", showSidebar);
+sidebarOpenBtn.addEventListener("click", toggleSidebar);
+sidebarCloseBtn.addEventListener("click", toggleSidebar);
